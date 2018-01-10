6212f59a86840fa7ee4e268ff651e487bc503058
Bruno Machado Brandão <brunin5010@gmail.com>
2018-01-10 09:27:16 -0200

    Inicio do thrust help

--- src/main/resources/cli/cli.js
+++ src/main/resources/cli/cli.js
@@ -6,14 +6,18 @@ var Collectors = Java.type("java.util.stream.Collectors");
 
 var CLI_COMMANDS = [ {
 	name : [ 'help' ],
-	args: [],
+	description: 'Show thrust help',
+	args: [{
+		name: 'cmd', 
+		description : 'Command name to show usage'
+	}],
 	options: [],
 	runner : cliHelp
 }, {
 	name : [ 'init' ],
+	description: 'Create a new Thrust app',
 	args : [ {
 		name : 'path',
-		required : false,
 		description : 'Path to create the seed'
 	} ],
 	options : [ {
@@ -24,6 +28,7 @@ var CLI_COMMANDS = [ {
 	runner : '/cli/cliInit'
 }, {
 	name : [ 'install' ],
+	description: 'Install or update bitcodes on a Thrust app',
 	args : [ {
 		name : 'bitcode',
 		description : 'Name of the bitcode to be installed.'
@@ -70,31 +75,33 @@ function runCLI(args) {
 }
 
 function cliHelp(runInfo) {
-	print('Thrust help')
+	print('usage: thrust <command> [<args>] [<options>]')
 	print()
 	
 	print('Available commands:')
 	print()
 	
 	CLI_COMMANDS.forEach(function(cliCmd) {
-		print('Name: ' + cliCmd.name.join(', '))
+		var cmdHelp = cliCmd.name.join(', ') + '   ' + cliCmd.description
+		print(cmdHelp)
 		
-		if (cliCmd.args.length > 0) {
-			print('Arguments: ')
-			print(cliCmd.args.map(function(arg) {
-				return arg.name + ' - ' + arg.description
-			}).join('\n'))
-		}
+//		if (cliCmd.args.length > 0) {
+//			print(cliCmd.args.map(function(arg) {
+//				var name = arg.required ? arg.name : '[' + arg.name + ']'
+//				return name + ' - ' + arg.description
+//			}).join(' '))
+//		}
 		
-		if (cliCmd.options && cliCmd.options.length > 0) {
-			print('Options: ')
-			print(cliCmd.options.map(function(opt) {
-				return opt.name.map(function(name) {
-					return '-'.concat(name)
-				}).join(', ') + '    ' + opt.description
-			}).join('\n'))
-		}
-		
-		print()
+//		if (cliCmd.options && cliCmd.options.length > 0) {
+//			print('Options: ')
+//			print(cliCmd.options.map(function(opt) {
+//				return opt.name.map(function(name) {
+//					return '-'.concat(name)
+//				}).join(', ') + '    ' + opt.description
+//			}).join('\n'))
+//		}
 	})
+	
+//	print()
+//	print('Use thrust <command> -h for specific help on some command')
 }
