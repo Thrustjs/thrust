a357c75a1ee74160f6301ad130c0b6467a74c159
Cleverson Puche <cleverson@softbox.com.br>
2019-02-12 08:53:57 -0200

    ajustando scripts para gerar instalador da nova versão com GraalVM

--- scripts/thrust.sh
+++ scripts/thrust.sh
@@ -1,47 +1,14 @@
 #!/bin/sh
 
-DEBUG=''
-GRAAL=false
 THRUSTDIR=/opt/thrust/lib/
 
-for i in "$@"
-do
-    case $i in
-        --debug)
-            DEBUG=true
-        ;;
-        --graal)
-            GRAAL=true
-        ;;
-        *)
-                # unknown option
-        ;;
-    esac
-done
-
-if [ "${GRAAL}" = true ] || [ "${USE_THRUST_GRAAL}" = true ]; then 
-    if [ "${GRAAL_HOME}" = "" ]; then
-        echo 'Error: To use thrust with graal, GRAAL_HOME must be set.' >&2
-        exit 1
-    fi
-
-    if [ "${DEBUG}" = true ]; then 
-        DEBUG='--inspect'
-    fi
-
-    eval $GRAAL_HOME/bin/js --jvm --strict $DEBUG $THRUSTDIR/thrust.js -- -GRAAL true -THRUSTDIR $THRUSTDIR $*
-else 
-    if [ "${DEBUG}" = true ]; then 
-        if ! [ -x "$(command -v ncdbg)" ]; then
-            echo 'Error: ncdbg não foi encontrado, vide seção de debug no README.' >&2
-            exit 1
-        fi
-
-        DEBUG='--J-agentlib:jdwp=server=y,transport=dt_socket,address=7777,suspend=y'
-
-        echo 'Iniciando NCDbg para debug...'
-        ncdbg &>/dev/null &
-    fi
-
-    eval jjs -strict --language=es6 $DEBUG $THRUSTDIR/thrust.js -- $*
+if [ "${GRAAL_HOME}" = "" ]; then
+    echo 'Error: GRAAL_HOME environment variable must be set. Download it from https://github.com/oracle/graal/releases' >&2
+    exit 1
 fi
+
+if [ "${DEBUG}" = true ]; then 
+    DEBUG='--inspect'
+fi
+
+eval $GRAAL_HOME/bin/js --jvm --polyglot $DEBUG $THRUSTDIR/thrust.js -- $*
