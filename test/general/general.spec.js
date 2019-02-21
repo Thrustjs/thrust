063f75f3f3b8fad4cb24b2d0c64e1286487da9ea
Cleverson Puche <cleverson@softbox.com.br>
2019-02-21 10:52:30 -0300

    ajustando a passagem da flag de compatibilidade com o nashorn

--- test/general/general.spec.js
+++ test/general/general.spec.js
@@ -1,6 +1,6 @@
 exports = function exec(describe, it, beforeEach, afterEach, expect, should, assert) {
-    describe('Testes gerais', function() {
-        it('getConfig should be frozen', function() {
+    describe('Testes gerais', function () {
+        it('getConfig should be frozen', function () {
             // expect(function() {
             //     let cfg = getConfig();
             //     cfg.a = 1;
@@ -9,6 +9,39 @@ exports = function exec(describe, it, beforeEach, afterEach, expect, should, ass
             cfg.a = 1
 
             expect(cfg.a).to.equals(undefined)
+        });
+    });
+
+    describe('Testes ES8 da Graal', function () {
+        const obj = {
+            a: 1
+        };
+
+        it('Teste com destrutor', function () {
+            const { a } = obj;
+            expect(a).to.equals(1);
+        });
+
+        it('Teste com arrow function', function () {
+            const sum = (x, y) => x + y;
+            expect(sum(1, 2)).to.equals(3);
+        });
+
+        it('Teste com StringInterpolation', function () {
+            const numberOne = 1
+            const stringInterpolation = `NumberOne: ${numberOne}`
+            expect(stringInterpolation).to.be.equals('NumberOne: 1')
+        });
+
+        it('Teste com outboxing não automático do Java para o JS', function () {
+            const JString = Java.type('java.lang.String');
+            const javaString = new JString('teste');
+            /**
+            * Se o outboxing acontecer automaticamente, dará erro na linha abaixo
+            * já que a String do JS não tem o método getBytes
+            **/
+            const javaByteArray = javaString.getBytes();
+            expect(javaByteArray.length).to.equals(5);
         });
     });
 }
